18f48a1ddcbf0ff4ab8e0d35ac064e11477aa6df
modwodmm <modwodmm2010@gmail.com>
2026-05-06 09:49:02 +0500

    Used switches instead of if statements and added comments

--- adventure_story/src/adventure_story/Main.java
+++ adventure_story/src/adventure_story/Main.java
@@ -7,12 +7,15 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-	    System.out.println("The Adventure Story of a young man starts here, he was born in travelers and he became a traveler. Today, you have the chance to choose his path. Now lets begin.");
-	    System.out.println("You bumped into a stranger, he is asking if he can teach you fishing\n 1.Learn fishing from stranger.\n 2.Ignore the stranger.");
+//Start of adventure		
+	    System.out.println("The Adventure Story of a young man starts here, he was born in travelers and he became one. Today, you have the chance to choose his path. Now lets begin.");
+	    System.out.println("You bumped into a stranger, he is asking if he can teach you fishing.\n 1.Learn fishing from stranger.\n 2.Ignore the stranger.");
 	    int choice1 = scanner.nextInt();
+//First choice	    
 	    if(choice1 == 1) {
 	    	System.out.println("You learned fishing and got a fishing rod from the stranger!\nAdvice: Got lucky this time, stay away from strangers.");
 	    	System.out.println("You are crossing a river!\n 1. Fish.\n 2. Continue adventure.");
+//Second Choice	    	
 	    	int choice2 = scanner.nextInt();
 	    	if(choice2 == 1) {
 	    		System.out.println("You caught a salmon!\nAdvice: This fish can be cooked for eating.");
@@ -25,23 +28,29 @@ public class Main {
 	    	System.out.println("You ignored the stranger!");
 	    }
 	    System.out.println("You were walking through a forest and found an axe beside a guy.\n 1. Take the axe without asking,\n 2. Ask the guy for the axe\n 3. Walk away.");
+//Third choice	    
 	    int choice3 = scanner.nextInt();
-	    if(choice3 == 1) {
-	    	System.out.println("You got an axe!\nAdvice: That is stealing.");
-	    }
-	    else if(choice3 == 2) {
-	    	System.out.println("The guy asked for 3 silver coin for the axe! (You got 0 silver coins)\n 1. Walk away.\n 2. Curse him.");
+	    switch(choice3) {
+	    case 1 : 
+	    	System.out.println("You took the axe without asking.\nAdvice: That is called stealing, its best if you don't do it.");
+	    	break;
+	    case 2 :
+	    	System.out.println("You asked the guy if you can take the axe. He asked for 7 silver coins.\n Inventory:\n- 5 silver coins\n- Fishing rod");
+	    	System.out.println("1. Buy the axe\n2. Curse him and walk away");
+//Forth choice	    	
 	    	int choice4 = scanner.nextInt();
 	    	if(choice4 == 1) {
-	    		System.out.println("You didn't buy the axe and walked away!");
+	    		System.out.println("You don't enough coins to buy the axe!");
 	    	}
 	    	else {
-	    		System.out.println("You cursed him\nAdvice: Manners, learn some manners.");
+	    		System.out.println("You cursed him and walked away.\nAdvice: Learn some manners!");
 	    	}
-	    }
-	    else {
+	    	break;
+	    case 3 : 
 	    	System.out.println("You walked away!");
+	    	break;
 	    }
+//End	    
 	    System.out.println("The adventure ends here. It shall continue later!");
 
 	}
